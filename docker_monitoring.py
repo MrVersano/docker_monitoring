94dde29a42b2fac878de507f07ff4b055eaa1b22
Kobi Versano <kobi.versano@imperva.com>
2022-07-18 10:48:15 +0300

    Rewrote the script

--- docker_monitoring.py
+++ docker_monitoring.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import os
+import docker
+import socket
 
 
 
@@ -19,26 +21,21 @@ def post_to_slack(channel_name, message):
 		'channel': channel_name,
 		'as_user': True,
 		'text': message,
-        'link_names': True
+		'link_names': True
 	}
 
 	requests.post(url='https://slack.com/api/chat.postMessage', data=data)
 
-# Configure server and port
-server = 'localhost'
-port = 2375
 
-# Pull a list of all containers from the Docker API
-r = requests.get(url=f'http://{server}:{port}/containers/json?all=1')
-containers_data = r.json()
+not_running_containers = [] # A list of containers that are not running
+client = docker.from_env()
+for container in client.containers.list():
+	if container.status != 'running':
+		not_running_containers.append(container.name)
 
-stopped_containers = []
+if len(not_running_containers) == 0:
+	print("All containers running.")
+else:
+	for container in not_running_containers:
+		post_to_slack("es_alert_errors", f"WARNING: Container {container} is not running on server {socket.gethostname()}")
 
-# Loop through containers to see if a container is not running
-for container in containers_data:
-    if container['State'] != 'running' and container['State'] != "restarting":
-        stopped_containers.append(container['Names'][0].replace("/", ""))
-
-# Post message to slack if any container is not running
-if len(stopped_containers) > 0:
-    post_to_slack('es_alert_errors', f'@here Docker Alert!\nThe following containers have stopped running on the reporting server: {stopped_containers}')
